5043ac238a23d908a3e63b4226cdc20d752c7566
BaguetteYeeter <baguetteyeeter@gmail.com>
2020-12-08 18:52:20 +0000

    Game is now playable

--- main.py
+++ main.py
@@ -12,6 +12,17 @@ width = 50
 #width of the player
 vel = 5
 #idk velocity maybe?
+cactusX = 608
+#X of the cactus
+cactusY = 416
+#Y of the cactus
+isJump = False
+#is the player jumping
+jumpCount = 8
+#jump count
+score = 0
+#set the score
+delayTime = 20
 
 pygame.init()
 #initialize pygame
@@ -25,7 +36,7 @@ run = True
 #should the loop run?
 
 while run:
-    pygame.time.delay(100)
+    pygame.time.delay(delayTime)
     #set the delay
 
     for event in pygame.event.get():
@@ -39,7 +50,41 @@ while run:
     pygame.draw.rect(mainScreen, (255,0,0), (x, y, width, height))
     #draw the player
 
+    cactusX = cactusX - 8
+    if cactusX <= 0:
+        cactusX = 608
+        score += 1
+
+    keys = pygame.key.get_pressed()
+    #make it simpler to get input
+
+    if keys[pygame.K_SPACE]:
+        isJump = True
+        #is the spacebar pressed?
+
+    if isJump:
+        if jumpCount >= -8:
+            y -= (jumpCount * abs(jumpCount)) * 0.5
+            #maths
+            jumpCount -= 0.5
+            #x=x-1
+        else:
+            jumpCount = 8
+            isJump = False
+            #reset the jump variables
+
+    if y > 366 and cactusX < 82:
+        run = False
+
+    pygame.draw.rect(mainScreen, (0, 200, 0), (cactusX, cactusY, 32, 32))
+
+    if score >= 100:
+        print("you fucking cheater")
+    else:
+        delayTime = int(20 - int(score/5))
+
     pygame.display.update()
     #update the display
+print(score)
 pygame.quit()
 #quit pygame
